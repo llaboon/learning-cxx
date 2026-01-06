bc2dd4cee39212017b6d7fec7aad6fd89e9325ba
binliu <87918526+llaboon@users.noreply.github.com>
2026-01-06 23:27:51 +0800

    Update main.cpp

--- exercises/14_class_destruct/main.cpp
+++ exercises/14_class_destruct/main.cpp
@@ -11,24 +11,21 @@ class DynFibonacci {
 
 public:
     // TODO: 实现动态设置容量的构造器
-    DynFibonacci(int capacity): cache(new size_t[capacity]{0,1}), cached(2) {
-        //cache[0]=0;
-        //cache[1]=1;
-        for(int i=2;i<capacity;i++)
-        cache[i]=cache[i-1]+cache[i-2];
+    DynFibonacci(int capacity): cache(new size_t[capacity]), cached(2) {
+        if (capacity>0)cache[0]=0;
+        if(capacity>1)cache[1]=1;
     }
 
     // TODO: 实现析构器，释放缓存空间
     ~DynFibonacci(){
-        delete cache;
-        //cache=0;
+        delete[] cache;
     }
 
     // TODO: 实现正确的缓存优化斐波那契计算
     size_t get(int i) {
-        /*for (; false; ++cached) {
+        for (; cached<=i; ++cached) {
             cache[cached] = cache[cached - 1] + cache[cached - 2];
-        }*/
+        }
         return cache[i];
     }
 };
@@ -37,6 +34,5 @@ int main(int argc, char **argv) {
     DynFibonacci fib(12);
     ASSERT(fib.get(10) == 55, "fibonacci(10) should be 55");
     std::cout << "fibonacci(10) = " << fib.get(10) << std::endl;
-    //fib.~DynFibonacci();
     return 0;
 }
